086b897d23ea2658b2d57c0e57b7d7a0ffa43116
Eyob Hailu <EyobHailu246@gmail.com>
2025-09-03 14:58:14 +0300

    final

--- data/index.js
+++
@@ -1,6 +0,0 @@
-export const gridItem = [
-  { title: 'Title1', description: 'Desc1', id: 1 },
-  { title: 'Title1', description: 'Desc1', id: 1 },
-  { title: 'Title1', description: 'Desc1', id: 1 },
-  { title: 'Title1', description: 'Desc1', id: 1 },
-];
